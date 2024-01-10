21aef3caa144bdc6dc64be65ff21efb352c6b4c8
Bilou4000 <claire.benes@artfx.fr>
2024-01-10 12:03:46 +0100

    [feat] movement

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    private Rigidbody rb;
+    private Vector3 input;
+    private Vector3 relative;
+    private Quaternion rotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GatherInput();
+        Look();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GatherInput()
+    {
+        input = new Vector3(UnityEngine.Input.GetAxisRaw("Horizontal"), 0, UnityEngine.Input.GetAxisRaw("Vertical"));
+    }
+
+    private void Look()
+    {
+        if (input != Vector3.zero)
+        {
+            relative = (transform.position + input) - transform.position;
+            rotate = Quaternion.LookRotation(relative, Vector3.up);
+
+            transform.rotation = rotate;
+        }
+
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(transform.position + (transform.forward * input.normalized.magnitude) * speed * Time.deltaTime);
+        //Vector3 movement = new Vector3(transform.forward.x * input.y * speed * Time.deltaTime,
+        //    0, transform.forward.z * input.y * speed * Time.deltaTime);
+        //rb.MovePosition(transform.position + movement);
+    }
+}
