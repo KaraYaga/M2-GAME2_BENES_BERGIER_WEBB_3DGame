f86ffcc265a2eb7731a89cdff9f7fe5398295fdc
Bilou4000 <claire.benes@artfx.fr>
2024-01-18 12:21:18 +0100

    [chore] minor modif

--- Assets/Scripts/Duck_Collection.cs
+++ Assets/Scripts/Duck_Collection.cs
@@ -100,7 +100,7 @@ public class Duck_Collection : MonoBehaviour
 
                 Vector3 throwDirection = (duckPrefab.transform.forward * throwForce * distance) + new Vector3(0, upForce, 0);
                 duckPrefab.GetComponent<Rigidbody>().AddForce(throwDirection, ForceMode.VelocityChange);
-                //currentDucks--;
+                currentDucks--;
 
                 UpdateDuckCountText();
             }
